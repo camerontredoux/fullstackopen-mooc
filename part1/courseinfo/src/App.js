f80370963b9492591f6f74640a1aaaf29dbe4c4e
Cameron Tredoux <camtredoux@gmail.com>
2021-10-19 12:34:38 -0600

    Refactoring and destructuring props

--- part1/courseinfo/src/App.js
+++ part1/courseinfo/src/App.js
@@ -1,64 +1,70 @@
 import React from "react"
 
-function Header(props) {
+const Header = ({ course }) => {
   return (
     <>
-      <h1>{props.course}</h1>
+      <h1>{course}</h1>
     </>
   )
 }
 
-function Part(props) {
+const Part = ({ part, exercises }) => {
   return (
     <>
       <p>
-        {props.part} {props.exercise}
+        {part} {exercises}
       </p>
     </>
   )
 }
 
-function Content(props) {
+const Content = ({ parts }) => {
   return (
     <>
-      <Part part={props.part[0].part} exercise={props.part[0].exercise} />
-      <Part part={props.part[1].part} exercise={props.part[1].exercise} />
-      <Part part={props.part[2].part} exercise={props.part[2].exercise} />
+      <Part part={parts[0].part} exercises={parts[0].exercises} />
+      <Part part={parts[1].part} exercises={parts[1].exercises} />
+      <Part part={parts[2].part} exercises={parts[2].exercises} />
     </>
   )
 }
 
-function Total(props) {
+const Total = ({ parts }) => {
   return (
     <>
       <p>
-        Number of exercises {props.exercises[0].exercise + props.exercises[1].exercise + props.exercises[2].exercise}
+        Number of exercises {parts[0].exercises + parts[1].exercises + parts[2].exercises}
       </p>
     </>
   )
 }
 
-export default function App() {
-  const course = 'Half Stack application development'
+const App = () => {
 
-  const part1 = 'Fundamentals of React'
-  const exercises1 = 10
-  const part2 = 'Using props to pass data'
-  const exercises2 = 7
-  const part3 = 'State of a component'
-  const exercises3 = 14
-
-  const parts = [
-    { part: part1, exercise: exercises1 },
-    { part: part2, exercise: exercises2 },
-    { part: part3, exercise: exercises3 }
-  ]
+  const course = {
+    name: 'Half Stack application development',
+    parts: [
+      {
+        part: 'Fundamentals of React',
+        exercises: 10
+      },
+      {
+        part: 'Using props to pass data',
+        exercises: 7
+      },
+      {
+        part: 'State of a component',
+        exercises: 14
+      }
+    ]
+  }
 
   return (
     <>
-      <Header course={course} />
-      <Content part={parts} />
-      <Total exercises={parts} />
+      <Header course={course.name} />
+      <Content parts={course.parts} />
+      <Total parts={course.parts} />
     </>
   );
 }
+
+export default App
