21522d968f009dd766d44f6d2fa539aecaab7928
Cameron Tredoux <camtredoux@gmail.com>
2021-10-23 17:29:29 -0600

    Feature: display anecdote with most votes

--- part1/anecdotes/src/App.js
+++ part1/anecdotes/src/App.js
@@ -1,5 +1,28 @@
 import React, { useState } from 'react'
 
+function MostVotes({ anecdotes, points }) {
+
+  let max = points[0]
+  let loc = 0
+
+  for (let point in points) {
+    if (points[point] > max) {
+      max = points[point]
+      loc = point
+    }
+  }
+  // for (let dote in props.anecdotes) {
+  //   console.log(props.anecdotes[dote]);
+  // }
+
+  return (
+    <>
+      <p>{anecdotes[loc]}</p>
+      <p>This anecdote has {max} votes</p>
+    </>
+  )
+}
+
 export default function App() {
   const anecdotes = [
     'If it hurts, do it more often',
@@ -11,7 +34,24 @@ export default function App() {
     'Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when diagnosing patients'
   ]
 
-  const [selected, setSelected] = useState(0)
+  const [selected, setSelected] = useState(Math.floor(Math.random() * anecdotes.length))
+  const [points, setPoints] = useState({
+    0: 0,
+    1: 0,
+    2: 0,
+    3: 0,
+    4: 0,
+    5: 0,
+    6: 0
+  })
+
+  function vote() {
+    const arr = {
+      ...points
+    }
+    arr[selected] += 1
+    setPoints(arr)
+  }
 
   function handleClick() {
     const random = Math.floor(Math.random() * anecdotes.length)
@@ -20,9 +60,13 @@ export default function App() {
 
   return (
     <div>
+      <h1>Anecdote of the Day</h1>
+      <p>{anecdotes[selected]}</p>
+      <p>This anecdote has {points[selected]} votes</p>
       <button onClick={handleClick}>next anecdote</button>
-      <br></br>
-      {anecdotes[selected]}
+      <button onClick={vote}>vote</button>
+      <h2>Anecdote with the most votes</h2>
+      <MostVotes anecdotes={anecdotes} points={points} />
     </div>
   )
 }
