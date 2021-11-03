2493bb9a170eda0999fad98c5fb600bae20f9746
Cameron Tredoux <camtredoux@gmail.com>
2021-11-02 23:59:44 -0600

    Fetch initial data from database and populate persons state

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,16 +1,17 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import Persons from "./components/Persons"
 import Filter from './components/Filter'
 import PersonForm from './components/PersonForm'
+import axios from "axios"
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', phone: '040-123456' },
-    { name: 'Ada Lovelace', phone: '39-44-5323523' },
-    { name: 'Dan Abramov', phone: '12-43-234345' },
-    { name: 'Mary Poppendieck', phone: '39-23-6423122' }
-  ])
-
+  const [persons, setPersons] = useState([])
+  useEffect(() => {
+    axios.get("http://localhost:3001/persons")
+      .then(res => {
+        setPersons(res.data)
+      })
+  }, [])
   const [newName, setNewName] = useState('')
   const [newPhone, setNewPhone] = useState('')
   const [search, setSearch] = useState('')
