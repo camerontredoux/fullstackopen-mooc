444e28921a29f07453bbffb226ff3beec50f9850
Cameron Tredoux <camtredoux@gmail.com>
2021-11-03 19:05:31 -0600

    Show detailed information when there is only one country being filtered

--- part2/countries/src/components/Country.js
+++ part2/countries/src/components/Country.js
@@ -1,10 +1,38 @@
 const Country = (props) => {
-  return (
-    <>
-      {props.name}
-      <br></br>
-    </>
-  )
+
+  const country = props.country
+  const languages = Object.values(country.languages).map(language => {
+    return <li key={language} > {language}</li >
+  })
+  const capitals = Object.values(country.capital).map(capital => {
+    return <li key={capital}>{capital}</li>
+  })
+
+  if (props.countries.length === 1) {
+    return (
+      <>
+        <h1>{country.name.common}</h1>
+        <div>
+          capital
+          <ul>
+            {capitals}
+          </ul>
+        </div>
+        <div>
+          population {country.population}
+        </div>
+        <h2>Languages</h2>
+        <ul>
+          {languages}
+        </ul>
+        <img src={country.flags.svg} alt="flag of country" width="200"></img>
+      </>
+    )
+  } else {
+    return (
+      <li key={country.name.common}>{country.name.common}</li>
+    )
+  }
 }
 
 export default Country
